39578acf1ed76c85b9c97b71fe4917b88cf0b369
Christian Mendoza <christianmendoza@Mac-mini-de-Christian.local>
2026-02-17 16:21:50 -0600

    sonarqube

--- Servicios/Ejemplo-Microservicios/eureka-server/src/test/java/com/ejemplo/eureka/EurekaServerApplicationTests.java
+++ Servicios/Ejemplo-Microservicios/eureka-server/src/test/java/com/ejemplo/eureka/EurekaServerApplicationTests.java
@@ -3,6 +3,7 @@ package com.ejemplo.eureka;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class EurekaServerApplicationTests {
 
--- Servicios/Ejemplo-Microservicios/saludo-service/src/test/java/com/ejemplo/saludo/SaludoServiceApplicationTests.java
+++ Servicios/Ejemplo-Microservicios/saludo-service/src/test/java/com/ejemplo/saludo/SaludoServiceApplicationTests.java
@@ -3,6 +3,7 @@ package com.ejemplo.saludo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class SaludoServiceApplicationTests {
 

4cd7543eee6b40b27bd89eb967f6ace8f8dd8d55
Christian Mendoza <christianmendoza@Mac-mini-de-Christian.local>
2026-02-17 16:31:22 -0600

    sonarqube

--- Servicios/Ejemplo-Microservicios/auth-service/src/test/java/com/ejemplo/auth/AuthServiceApplicationTests.java
+++ Servicios/Ejemplo-Microservicios/auth-service/src/test/java/com/ejemplo/auth/AuthServiceApplicationTests.java
@@ -5,7 +5,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
-@SpringBootTest
+@SpringBootTest(
+    properties = {
+        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration"
+    }
+)
 class AuthServiceApplicationTests {
 
 	@Test
